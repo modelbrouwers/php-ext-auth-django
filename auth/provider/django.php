0cc07d67a30e19d11f3bedb46a3773345c74c344
Sergei Maertens <sergei@maykinmedia.nl>
2016-03-12 10:51:33 +0100

    Implemented the autologin functionality

--- auth/provider/django.php
+++ auth/provider/django.php
@@ -77,12 +77,14 @@ class django extends \phpbb\auth\provider\base
      * @param   string                    $php_ext            PHP file extension
      */
     public function __construct(
+        \phpbb\db\driver\driver_interface $db,
         \phpbb\config\config $config,
         \phpbb\request\request $request,
         \phpbb\user $user,
         $phpbb_root_path,
         $php_ext
     ) {
+        $this->db = $db;
         $this->config = $config;
         $this->request = $request;
         $this->user = $user;
@@ -127,7 +129,14 @@ class django extends \phpbb\auth\provider\base
      */
     public function login($username = null, $password = null)
     {
-        throw new Exception('not implemented');
+        $django_user = $this->get_django_user();
+
+        // Fallback, not logged in
+        return array(
+            'status'    => LOGIN_ERROR_EXTERNAL_AUTH,
+            'error_msg' => 'LOGIN_ERROR_EXTERNAL_AUTH',
+            'user_row'  => array('user_id' => ANONYMOUS),
+        );
     }
 
     /**
@@ -136,7 +145,56 @@ class django extends \phpbb\auth\provider\base
      */
     public function autologin()
     {
-        throw new Exception('not implemented');
+        $django_user = $this->get_django_user();
+        if (!$django_user) {
+            return array();
+        }
+
+        $username = $django_user['username']; // can be multibyte
+        $email = $django_user['email'];
+
+        set_var($username, $username, 'string', true);
+
+        $username_clean = utf8_clean_string($username);
+
+        $sql = sprintf(
+            'SELECT * FROM %1$s WHERE username_clean = \'%2$s\'',
+            USERS_TABLE,
+            $this->db->sql_escape($username_clean)
+        );
+        $result = $this->db->sql_query($sql);
+        $row = $this->db->sql_fetchrow($result);
+        $this->db->sql_freeresult($result);
+
+        // user exists
+        if($row) {
+            // check for inactive users
+            if($row['user_type'] == USER_INACTIVE || $row['user_type'] == USER_IGNORE) {
+                return array();
+            }
+
+            // success
+            return $row;
+        }
+
+        // user does not exist atm, we'll fix that
+        if(!function_exists('user_add'))
+        {
+            include($this->phpbb_root_path . 'includes/functions_user.' . $this->php_ext);
+        }
+
+        user_add($this->create_new_user($username, $email));
+
+        // get the newly created user row
+        // $sql already defined some lines before
+        $result = $this->db->sql_query($sql);
+        $row = $this->db->sql_fetchrow($result);
+        $this->db->sql_freeresult($result);
+
+        if($row) {
+            return $row;
+        }
+        return array();
     }
 
     /**
@@ -200,5 +258,36 @@ class django extends \phpbb\auth\provider\base
         }
 
         return null;
+    }
+
+    /**
+     * This function generates an array which can be passed to the user_add function in order to create a user
+     *
+     * @param   string  $username   The username of the new user.
+     * @param   string  $email      The e-mail address of the new user.
+     * @return  array               Contains data that can be passed directly to the user_add function.
+     */
+    private function create_new_user($username, $email)
+    {
+        // first retrieve default group id
+        $sql = sprintf('SELECT group_id FROM %1$s WHERE group_name = \'%2$s\' AND group_type = \'%3$s\'', GROUPS_TABLE, $this->db->sql_escape('REGISTERED'), GROUP_SPECIAL);
+        $result = $this->db->sql_query($sql);
+        $row = $this->db->sql_fetchrow($result);
+        $this->db->sql_freeresult($result);
+
+        if(!$row) {
+            trigger_error('NO_GROUP');
+        }
+
+        // generate user account data
+        return array(
+            'username'      => $username,
+            'user_password' => '',
+            'user_email'    => $email,
+            'group_id'      => (int)$row['group_id'],
+            'user_type'     => USER_NORMAL,
+            'user_ip'       => $this->user->ip,
+            'user_new'      => ($this->config['new_member_post_limit']) ? 1 : 0,
+        );
     }
 }
